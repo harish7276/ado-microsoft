b08317a4757ffa1667abbd6a220a4319be5d3a22
Yarra Harish (FCA) <t7588hy@cag.cdroot.net>
2025-04-11 22:25:21 +0530

    modified github file and vnet file

--- dev/main.tf
+++ dev/main.tf
@@ -4,9 +4,13 @@ resource "azurerm_virtual_network" "example" {
   location            = "West Europe"
   resource_group_name = "sanofi-dev-rg"
   address_space       = ["10.0.0.0/16"]
-#   dns_servers         = ["10.0.0.4", "10.0.0.5"]
+}
 
-
-  }
+resource "azurerm_virtual_network" "dev" {
+  name                = "dev-vnet"
+  location            = "West Europe"
+  resource_group_name = "sanofi-dev-rg"
+  address_space       = ["10.1.0.0/16"]
+}
 
 
