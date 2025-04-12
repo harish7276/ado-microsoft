e2bddcc655a0c2e58b694ec70cda033daf2d5273
Yarra Harish (FCA) <t7588hy@cag.cdroot.net>
2025-04-12 14:24:10 +0530

    key vault added

--- modules/key_vault/variables.tf
+++ modules/key_vault/variables.tf
@@ -15,14 +15,14 @@ variable "key_vaults" {
   description = "Define the key vaults to be created"
   type = map(object({
     name   = string
-    kv_sku = optional(string, "standard")
+    kv_sku = optional(string)
 
-    enabled_for_deployment          = optional(bool, false)
-    enabled_for_disk_encryption     = optional(bool, false)
-    enabled_for_template_deployment = optional(bool, false)
-    enable_rbac_authorization       = optional(bool, true)
-    soft_delete_retention_days      = optional(number, 7)
-    public_network_access_enabled   = optional(bool, true)
+    enabled_for_deployment          = optional(bool)
+    enabled_for_disk_encryption     = optional(bool)
+    enabled_for_template_deployment = optional(bool)
+    enable_rbac_authorization       = optional(bool)
+    soft_delete_retention_days      = optional(number)
+    public_network_access_enabled   = optional(bool)
 
 
     network_acls = optional(object({
