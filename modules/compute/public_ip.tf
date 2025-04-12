78f1324a9344168ad0876d9b3a5954c6ae17cad7
Yarra Harish (FCA) <t7588hy@cag.cdroot.net>
2025-04-12 13:25:30 +0530

    module location added

--- modules/compute/public_ip.tf
+++
@@ -1,7 +0,0 @@
-resource "azurerm_public_ip" "public_ip" {
-  count               = var.environment == "prod" ? 2 : 1
-  name                = "${var.environment}-public-ip-${count.index + 1}"
-  resource_group_name = var.resource_group_name
-  location            = var.location
-  allocation_method   = "Static"
-}
