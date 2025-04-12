3f40ed6147fca9e906a962fb042dbe42f3c846c5
Yarra Harish (FCA) <t7588hy@cag.cdroot.net>
2025-04-12 15:49:53 +0530

    key vault added

--- dev/key_vault.tf
+++ dev/key_vault.tf
@@ -1,17 +1,17 @@
-# data "azurerm_key_vault" "main" {
-#   name                = "sanofi-keyvault"
-#   resource_group_name = "sanofi-dev-rg"
-# }
+data "azurerm_key_vault" "main" {
+  name                = "sanofi-keyvault"
+  resource_group_name = "sanofi-dev-rg"
+}
 
-# data "azurerm_key_vault_secret" "admin_username" {
-#   name         = "vm-admin-username"
-#   key_vault_id = data.azurerm_key_vault.main.id
-# }
+data "azurerm_key_vault_secret" "admin_username" {
+  name         = "vm-admin-username"
+  key_vault_id = data.azurerm_key_vault.main.id
+}
 
-# data "azurerm_key_vault_secret" "admin_password" {
-#   name         = "vm-admin-password"
-#   key_vault_id = data.azurerm_key_vault.main.id
-# }
+data "azurerm_key_vault_secret" "admin_password" {
+  name         = "vm-admin-password"
+  key_vault_id = data.azurerm_key_vault.main.id
+}
 
 # module "key_vaults" {
 #   source = "../modules/key_vault"
