7b074ed91a61b365d9595308fd5977894338484a
Yarra Harish (FCA) <t7588hy@cag.cdroot.net>
2025-04-12 14:29:31 +0530

    key vault added

--- dev/terraform.tf
+++ dev/terraform.tf
@@ -2,6 +2,7 @@
 # TERRAFORM CONFIG
 ##################################################################################
 terraform {
+    experiments = [module_variable_optional_attrs]
     required_providers {
         azurerm = {
             source = "hashicorp/azurerm"
